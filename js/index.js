1672e83c6d59e27fd963c66928ea16a615af2c06
Ankeet Mourya <mouryaankeet@gmail.com>
2024-06-03 13:54:19 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function toggleMenu(){
+    var menu = document.getElementById("mobile-menu");
+
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+document.getElementById("menu-icon").addEventListener("click", function(){
+    toggleMenu();
+});
+
+document.getElementById("close").addEventListener("click", function(){
+    toggleMenu();
+});
